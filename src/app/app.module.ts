4f9ac78ed38035cbe4d2f77addbda75d4e3fa30c
Cocotle <luisdavidcocoyuv@gmail.com>
2023-09-15 09:40:52 -0600

    app-routing missing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { WorkExperienceService } from './services/work-experience.service';
+
+import { HttpClientModule } from '@angular/common/http';
 import { SkillsComponent } from './skills/skills.component';
 import { CertificationsComponent } from './certifications/certifications.component';
-import { InterestsComponent } from './interests/interests.component';
+import { InterestsComponent } from './interests/interests.component'
 
 @NgModule({
   declarations: [
@@ -18,9 +22,13 @@ import { InterestsComponent } from './interests/interests.component';
     InterestsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    WorkExperienceService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
