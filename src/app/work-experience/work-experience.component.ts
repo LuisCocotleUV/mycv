759cab2b1185154bdeec03ffe054c6496d343b22
LuisCocotleUV <113140993+LuisCocotleUV@users.noreply.github.com>
2023-09-14 22:04:34 -0600

    Work experience (#7)
    * Create master_ci_cd.yml
    
    * created header branch
    
    * created work-experience branch
    
    * work-experience again

---
+++ src/app/work-experience/work-experience.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { WorkExperienceService } from '../services/work-experience.service';
+
+@Component({
+  selector: 'app-work-experience',
+  templateUrl: './work-experience.component.html',
+  styleUrls: ['./work-experience.component.css']
+})
+export class WorkExperienceComponent implements OnInit {
+
+  trabajos: any[] = [];
+
+  constructor(private workExperienceService: WorkExperienceService) { }
+
+  ngOnInit(): void {
+    this.workExperienceService.getWorkExperience().subscribe((data: any[]) => {
+      console.log(data);
+      this.trabajos = data;
+    });
+  }
+}
